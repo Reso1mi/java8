83355eee087ae7ea5cc941e7cea951d13a0f1200
lgw <privateli@qq.com>
2019-09-18 00:01:45 +0800

    predicate&supplier

---
+++ src/main/java/top/imlgw/java8/predicate/BiPredicateTest.java
@@ -0,0 +1,17 @@
+package top.imlgw.java8.predicate;
+
+import java.util.function.BiPredicate;
+
+/**
+ * @author imlgw.top
+ * @date 2019/9/17 23:57
+ */
+public class BiPredicateTest {
+    public static void main(String[] args) {
+        System.out.println(compare(2323,33124,(a,b)->a.toString().length()>b.toString().length()));
+    }
+
+    public static boolean compare(Integer a, Integer b, BiPredicate<Integer,Integer> biPredicate){
+        return biPredicate.test(a,b);
+    }
+}
